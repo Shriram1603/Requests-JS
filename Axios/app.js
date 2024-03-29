ce8ae673f03c5f1c480c53b9e8f8dee9199650c8
Shriram.S.S <shriram.s.s@ShriramSSs-MacBook-Pro.local>
2024-03-29 19:51:37 +0530

    practice done

--- Axios/app.js
+++ Axios/app.js
@@ -1,1 +1,47 @@
-axios.get("https://swapi.dev/api/people/1");
+// axios.get("https://swapi.dev/api/people/1")
+// .then((res)=>{
+//     console.log("Resolved :",res);
+// })
+
+const getStarWars = async (id) =>{
+    try{
+
+        const res= await axios.get(`https://swapi.dev/api/people/${id}`);
+        console.log(res.data);
+    }
+    catch(e){
+        console.log("erroe !!",e)
+    }
+};
+
+// getStarWars(6);
+// getStarWars(10);
+// getStarWars(100);
+
+const button =document.querySelector('button');
+
+const addNewJoke = async () =>{
+    
+    const joketext = await getDadJoke();
+    const newLi=document.createElement('li');
+    newLi.append(joketext);
+    jokes.append(newLi);
+    
+}
+
+const jokes=document.querySelector("#jokes")
+const getDadJoke = async () =>{
+    try{
+
+        const config = {headers :{Accept : "application/json"}}
+        const res= await axios.get("https://icanhazdadjoke.com/",config);
+        // console.log(res.data.joke);
+        return res.data.joke;
+    }
+    catch(e){
+        return"Sorry no jokes available !! :(";
+    }
+    
+}
+
+button.addEventListener('click',addNewJoke);
